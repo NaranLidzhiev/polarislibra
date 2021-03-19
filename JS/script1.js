c20513cb68c1d60006af28309f3d0ecc0749ef32
Naran <lidzhiev-03@mail.ru>
2021-03-19 23:51:12 +0300

    yep we did it

--- JS/script1.js
+++ JS/script1.js
@@ -23,17 +23,22 @@
 
 'use strict';
 const numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
- PERSONALlMovieDB :
+ const PERSONALlMovieDB =
 {
-    count: numberofFilms
-    movies: null
-    actors: null
-    genres: null
-    privat: false
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 }
-const lastone = prompt("Один из последних просмотренных фильмов", "");
-const revue = prompt("На сколько оцените его?", "")
-movies:
-{
-    lastone: revue
-}
+
+const a = prompt  ("Один из последних просмотренных фильмов", ""),
+      b = +prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = +prompt("На сколько оцените его?", "");
+      
+PERSONALlMovieDB.movies[a] = b;
+PERSONALlMovieDB.movies[c] = d;
+
+console.log(PERSONALlMovieDB);
+
