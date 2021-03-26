2700302b247b7ee1446a177a21c80859172290ce
Naran <lidzhiev-03@mail.ru>
2021-03-27 01:04:56 +0300

    другой способ подсчета ключей объекта

--- JS/script1.js
+++ JS/script1.js
@@ -20,23 +20,28 @@ const options = {
     colors: {
        border: 'black',
        bg: 'red'
-    }
+    },
+    
 }
+
+
+console.log(Object.keys(options).length);
+
 //console.log(options["colors"]["border"]);
 
 // delete options.name;
 // console.log(options);
-let counter =0;
-for (let key in options)
-{
-   if (typeof(options[key]) === 'object')
-   {
-      for (let i in options[key])
-      {
-         console.log( `Свойство ${key} имеет значение ${options[key][i]}`);
-         counter++
-      }
-   } else  {console.log( `Свойство ${key} имеет значение ${options[key]}`); counter++;}
+// let counter =0;
+// for (let key in options)
+// {
+//    if (typeof(options[key]) === 'object')
+//    {
+//       for (let i in options[key])
+//       {
+//          console.log( `Свойство ${key} имеет значение ${options[key][i]}`);
+//          counter++
+//       }
+//    } else  {console.log( `Свойство ${key} имеет значение ${options[key]}`); counter++;}
    
-}
-console.log(counter);
+// }
+//console.log(counter);
