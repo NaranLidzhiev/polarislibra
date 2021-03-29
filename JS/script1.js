d0d104a6825fc9405a4976fb2c7bc65e94070a14
Naran <lidzhiev-03@mail.ru>
2021-03-29 23:49:59 +0300

    создание объекта и назначение ему прототипа

--- JS/script1.js
+++ JS/script1.js
@@ -20,3 +20,21 @@ console.log(typeof(str));
 console.log(typeof(strObj));
 
 console.dir([1,2,3]);
+
+const soldier = {
+   health: 400,
+   armor:100,
+   sayHello: function ()
+   {
+      console.log("Hello");
+   }
+};
+
+const jonh = {
+   health: 100,
+};
+
+// jonh.__proto__ = soldier;
+ Object.setPrototypeOf(jonh, soldier);
+
+jonh.sayHello();
