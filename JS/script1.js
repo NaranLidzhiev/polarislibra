12ea1cb62961bf1621fdc338fecf5aff2397b09b
Naran <lidzhiev-03@mail.ru>
2021-03-30 23:08:56 +0300

    примеры динамической типизации данных(приведение к какому-то определенному типу через различные преобразования

--- JS/script1.js
+++ JS/script1.js
@@ -1,91 +1,53 @@
-/* Задание на урок:
-
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
 'use strict';
-let numberofFilms;
-// Код возьмите из предыдущего домашнего задания
-const personalMovieDB = {
-   count: numberofFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false,
-   start: function () {
-      personalMovieDB.count = +prompt("сколько фильмов вы уже просмотрели?", '');
-      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-         personalMovieDB.count = +prompt("сколько фильмов вы уже просмотрели?", '');
-      }
 
-   },
+// To String
 
-   remembermyfilms: function () {
-      for (let i = 0; i < 2; i++) {
-         const a = prompt('Один из последних просмотренных фильмов', ''),
-            b = +prompt('На сколько оцените его ?', '');
+// 1) 
+console.log(typeof(String(143)));
+// 2)
+console.log(typeof(5+ ''));
 
-         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-         } else {
-            console.log('error');
-            i--;
-         }
-      }
-   },
+const num = 5;
 
-   detectPersonalLevel: function () {
-      if (personalMovieDB.count < 10) {
-         console.log("Просмотрено удивительно малое количество фильмов ");
-      } else
-      if (personalMovieDB.count < 30 && personalMovieDB.count > 10) {
-         console.log("Вы средний класс ");
-      } else
-      if (personalMovieDB.count > 30) {
-         console.log("Вы задрот ");
-      }
-   },
+console.log(`https://vk.com/catalog/${num}` );
 
-   writeYourGenres: function (likes) {
-      for (let i = 0; i < 3; i++) {
-         likes[i] = prompt(`Ваш любимый жанр под номером ${i+1}`, '')
-         if (likes[i] != null  && likes[i]  != '' && likes.length < 50) {
-            console.log('done');
-         } else {
-            console.log('error');
-            i--;
-         }
-      }
-      likes.forEach(element => console.log(element));
-   },
+const fornSize = 26 + 'px';
 
-   showMyDB: function (doza) {
-         if (!doza.privat) {
-            console.log(personalMovieDB);
-         }
-   },
+// To Number
+//1)
+console.log(typeof(Number('4')));
 
-   toggleVisibleMyDB: function (hugo) {
-      if (!hugo ) {
-         hugo = 'true';
-      } else {
-         hugo ='false';
-      };
-      console.log(hugo);
-   },
+//2)
 
-};
+console.log(typeof(+'4'));
 
-personalMovieDB.writeYourGenres(personalMovieDB.genres);
-personalMovieDB.showMyDB(personalMovieDB.privat);
-personalMovieDB.toggleVisibleMyDB(personalMovieDB.privat);
+//3)
+console.log(typeof(parseInt("15px", 10)));
+
+let answ = +prompt("Hello", "");
+
+// To boolean
+
+//0, '', null , undefined, NaN; все эти ребята превращаются в falls в типе boolean
+
+let switcher = null;
+
+if (switcher)
+{
+   console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher)
+{
+   console.log('Working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+//3)
+ console.log(typeof(!!"44444"));
+ 
