9e0b14de56777d647cc5bd0aea64dc004c04c4fa
Naran <lidzhiev-03@mail.ru>
2021-04-02 20:55:43 +0300

    стилизация, преобразования и прочие штуки со стилями

--- JS/script1.js
+++ JS/script1.js
@@ -1,23 +1,32 @@
 
 'use strict';
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
 
-console.log(box);
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
 
-const btns = document.getElementsByTagName('button');
+box.style.cssText = 'background-color: blue; width: 500px';
 
-console.log(btns[0]);
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-const circles =  document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
+for (let i =0; i < hearts.length; i++)
+{
+    hearts[i].style.backgroundColor = 'aqua';
+}
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor ='maroon';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div'); // создаем элемент только в js файле
+//const text = document.createTextNode('Тут был я');
 
+div.classList.add('black');
+
+document.body.append(div);
