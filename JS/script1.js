1199fc2fd0e902f7239adbd674b298fdb7768b60
Naran <lidzhiev-03@mail.ru>
2021-04-03 17:59:28 +0300

    Задание по сайту

--- JS/script1.js
+++ JS/script1.js
@@ -1,49 +1,32 @@
 
 'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'), //идут от wrapper
-      oneHeart = wrapper.querySelector('.heart'); // идут от wrapper
-      
 
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+'use strict';
 
-box.style.cssText = 'background-color: blue; width: 500px';
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-for (let i =0; i < hearts.length; i++)
+const add = document.getElementsByClassName('promo__adv'),
+      genre = document.querySelector(".promo__genre"),
+      films = document.getElementsByClassName("promo__interactive-item"),
+      bg = document.getElementById('promo__bg');
+for (let i =0; i < add.length; i++)
 {
-    hearts[i].style.backgroundColor = 'aqua';
+    add[i].remove();
+} 
+genre.innerHTML = "Драма";
+
+for (let i =0; i< films.length; i++ )
+{
+    films[i].after(i+1);
 }
 
-hearts.forEach(item => {
-    item.style.backgroundColor ='maroon';
-});
-
-const div = document.createElement('div'); // создаем элемент только в js файле
-//const text = document.createTextNode('Тут был я');
-
-div.classList.add('black'); // придаем элементу класс со значением black
-
-wrapper.append(div);//после
-//wrapper.appendChild(div);
-// wrapper.prepend(div);//до
-// //hearts[0].before(div);  вставка до определенного элемента
-// hearts[0].after(div); // вставка после определенного элемента
-//wrapper.insertBefore(div, hearts[1]);//старый способ вставки до определенного элемента
-//wrapper.removeChild(hearts[1]); //старый способ удаления
-//wrapper.replaceChild(circles[0], hearts[0]); старый способ замены элементов
-// //circles[0].remove(); удаление элемента
-hearts[0].replaceWith(circles[0]); // замена элементов
-
-div.innerHTML = "<h1>Hello world</h1>";
-
-div.insertAdjacentHTML("afterend",'<h2>Hello</h2>'); // вставка html кода  после элемента 
-
-
+bg.style.backgroundImage ='url("img/bg.jpg")';
