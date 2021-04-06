d5cb235f24ad9dc4c641e3bc26e3923773ae23df
Naran <lidzhiev-03@mail.ru>
2021-04-06 16:30:44 +0300

    События и их обработчики

--- JS/script1.js
+++ JS/script1.js
@@ -1,54 +1,33 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
 
 
 'use strict';
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+// btn.onclick = function()
+// {
+//     alert('Clicker');
+// };
+let i =0;
+const deleteElement = (e) => { 
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if(i == 1)
+    // {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
 };
 
-const add = document.querySelectorAll('.promo__adv img'),
-    movieList = document.querySelector('.promo__interactive-list'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector(".promo__genre"),
-    films = document.getElementsByClassName("promo__interactive-item");
+// overlay.addEventListener('click', deleteElement);
 
-
-add.forEach(item => {
-    item.remove();
+btns.forEach(  btn => {
+   btn.addEventListener('click', deleteElement, {once: true});
 });
-movieList.innerHTML = "";
-genre.textContent = 'Драма';
-for (let i = 0; i < films.length; i++) {
-    films[i].after(i + 1);
-}
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-movieList.innerHTML = "";
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += ` 
-    <li class="promo__interactive-item">${i +1} ${film}
-    <div class="delete"></div>
-    </li>
-    `;
+const link = document.querySelector('a');
+link.addEventListener('click', (event)=>{
+  event.preventDefault();
+  console.log(event.target);
 });
+
