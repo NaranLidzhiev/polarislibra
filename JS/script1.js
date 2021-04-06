552611278bc14e9ab7018ae7dbfebcab9c113ae5
Naran <lidzhiev-03@mail.ru>
2021-04-06 20:54:29 +0300

    DOMать

--- JS/script1.js
+++ JS/script1.js
@@ -1,33 +1,12 @@
-
-
-'use strict';
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
-
-// btn.onclick = function()
-// {
-//     alert('Clicker');
-// };
-let i =0;
-const deleteElement = (e) => { 
-    console.log(e.target);
-    console.log(e.type);
-    // i++;
-    // if(i == 1)
-    // {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
-
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(  btn => {
-   btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-link.addEventListener('click', (event)=>{
-  event.preventDefault();
-  console.log(event.target);
-});
-
+// console.log(document.body.firstElementChild);
+//console.log(document.documentElement)
+// console.log(document.body.childNodes);
+// console.log(document.body.lastchild);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
