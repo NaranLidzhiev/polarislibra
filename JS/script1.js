9ce1c5f2fa89d8ba553be250542fe7e87f447337
Naran <lidzhiev-03@mail.ru>
2021-04-16 19:40:45 +0300

    скрипт, добавляющий класс в случае отсутствия и удаляющий в случае наличия №2

--- JS/script1.js
+++ JS/script1.js
@@ -10,10 +10,12 @@ const btns = document.querySelectorAll('button');
 // }
 
 btns[0].addEventListener('click', ()=>{
-    if (!btns[1].classList.contains('red'))
-    {
-        btns[1].classList.add('red');
-    } else {
-        btns[1].classList.remove('red');
-    }
+    // if (!btns[1].classList.contains('red'))
+    // {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
 });
