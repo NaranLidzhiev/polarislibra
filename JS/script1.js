eb5305ee6325372c92162e15492f2bbafa9d9ec6
Naran <lidzhiev-03@mail.ru>
2021-04-16 20:04:03 +0300

    деллигирование с помощью match

--- JS/script1.js
+++ JS/script1.js
@@ -21,9 +21,17 @@ btns[0].addEventListener('click', ()=>{
     btns[1].classList.toggle('red');
 });
 
-//console.log(btns[0].className);
+console.log(btns[0].className);
 wrapper.addEventListener('click', (event) =>{
-    if(event.target && event.target.tagName == "BUTTON"){//Tagname таргета является именем таргета , только в верхнем регистре
+    if(event.target && event.target.matches("button.red")){//Tagname таргета является именем таргета , только в верхнем регистре
         console.log('HELLO');
     }
-})
+});
+// btns.forEach(btn =>{
+//     btn.addEventListener('click', ()=>{
+//         console.log('Hello');
+//     });
+// });
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
