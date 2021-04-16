c5bf619ee492a6ab3e2c6314d4ba5dbaee442c57
Naran <lidzhiev-03@mail.ru>
2021-04-16 17:48:35 +0300

    работа с мобильной версией сайта

--- JS/script1.js
+++ JS/script1.js
@@ -1,113 +1,39 @@
-/* Задания на урок:
+//touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
+        console.log('Start');
+        console.log(e. targetTouches);
 
 
-// Возьмите свой код из предыдущей практики
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
-
-    const adv = document.querySelectorAll('.promo__adv img'),
-        movieList = document.querySelector('.promo__interactive-list'),
-        poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector(".promo__genre"),
-        films = document.getElementsByClassName("promo__interactive-item"),
-        addForm = document.querySelector('form.add'),
-        addInput = addForm.querySelector('.adding__input'),
-        checkbox = addForm.querySelector('[type="checkbox"]');
-    addForm.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
-
-        if(newFilm)
-        {
-            if(newFilm.length > 21)
-            {
-                newFilm = `${newFilm.substring(0,22)}...`;
-            }
-
-            if(favorite)
-            {
-                console.log("Добавляем любимый фильм");
-            }
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-            createMovieList(movieDB.movies, movieList);
-        }
-        event.target.reset();
     });
-    
-    const deleteAdv = (arr) => {
-        arr.forEach(item => {
-            item.remove();
-        });
-    };
-    deleteAdv(adv);
 
-    genre.textContent = 'Драма';
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-    poster.style.backgroundImage = 'url("img/bg.jpg")';
-    
-    
+        console.log(e.targetTouches[0].pageX);
 
-    const makeChanges = () => 
-    {
-        genre.textContent = 'Драма';
+    });
 
-        poster.style.backgroundImage = 'url("img/bg.jpg")';
-    };
+     box.addEventListener('touchend', (e) => {
+          e.preventDefault();
 
-    makeChanges();
+          console.log('endless');
 
-    const sortArr = (arr) => {
-        arr.sort();
-    };
+      });
 
+ });
 
-
-    function createMovieList(films, parent){
-        parent.innerHTML = "";
-        sortArr(films);
-        
-        films.forEach((film, i) => {
-            parent.innerHTML += ` 
-            <li class="promo__interactive-item">${i +1} ${film}
-            <div class="delete"></div>
-            </li>
-            `;
-        });
-
-        document.querySelectorAll('.delete').forEach((btn, i) => {
-            btn.addEventListener('click', ()=>{
-                btn.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-                createMovieList(films, parent);
-            });
-        });
-    }
-    createMovieList(movieDB.movies, movieList);
-});
+// touches 
+// targetTouches
+// changedTouches
+// https://hammerjs.github.io/ сайт который подгонит код под мобильные устройства
