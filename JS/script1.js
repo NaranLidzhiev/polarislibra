5000046dfd7e44e5c91e0f5dcc9b3ab223615882
Naran <lidzhiev-03@mail.ru>
2021-04-18 17:08:51 +0300

    timer 1

--- JS/script1.js
+++ JS/script1.js
@@ -1,40 +1,6 @@
-window.addEventListener('DOMContentLoaded', () => {
+const timerId = setTimeout(logger, 2000);
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-   function hideTabContent(){//скрываем табы
-       tabsContent.forEach(item =>{
-           item.classList.add('hide');
-           item.classList.remove('show');
-       });
+function logger (){
+    console.log('text');
+}
 
-       tabs.forEach(item  => {
-           item.classList.remove('tabheader__item_active');
-       });
-   }
-
-   function showTabContent(i) {
-       tabsContent[i].style.display = 'block';
-       tabs[i].classList.add('tabheader__item_active');
-   }
-
-   hideTabContent();
-
-   
-
-   tabsParent.addEventListener('click', (event )=> {
-       const target = event.target;
-       if(target && target.classList.contains('tabheader__item'))
-       {
-           tabs.forEach((item, i)=>{
-               if (target == item){
-                   hideTabContent();
-                   showTabContent(i);
-               }
-           });
-       }
-
-   });
-
-});
