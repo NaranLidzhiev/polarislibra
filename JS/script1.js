0edf9f82cb711b4b7f2611df3556b798426742cc
Naran <lidzhiev-03@mail.ru>
2021-04-20 22:16:05 +0300

    Параметры документа, работа с окнами

--- JS/script1.js
+++ JS/script1.js
@@ -4,3 +4,12 @@ box.addEventListener('click', ()=>{
     box.style.height = box.scrollHeight + 'px';
 });
 console.log("показ контента это wmd");
+
+console.log(box.getBoundingClientRect);// получение всех координатов, начиная от верхнего правого угла
+
+const style = window.getComputedStyle(box);//получаем стиль box
+console.log(style);
+console.log(style.display);
+
+
+console.log(document.documentElement.clientWidth);//document element при такой работе необходим
