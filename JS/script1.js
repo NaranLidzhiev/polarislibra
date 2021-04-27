ed96e630eeaf4fbcc44e43c98a808fc744af6796
Naran <lidzhiev-03@mail.ru>
2021-04-27 22:57:04 +0300

    контекст вызова 3

--- JS/script1.js
+++ JS/script1.js
@@ -1,13 +1,24 @@
 'use strict'
 
-const obj = {
-  a:20,
-  b:15,
-  sum: function () {
-      console.log(this);
-      function lapaka(){
-          console.log(this);//выводит undefined тк lapaka простая функция 
-      }
-  }
-};
-obj.sum();
+// const obj = {
+//   a:20,
+//   b:15,
+//   sum: function () {
+//       console.log(this);
+//       function lapaka(){
+//           console.log(this);//выводит undefined тк lapaka простая функция 
+//       }
+//   }
+// };
+// obj.sum();
+
+function User(name, id){
+    this.name = name;
+    this.id = id;
+    this.Human = true;
+    this.hello = function(){
+        console.log(`Hello ${name}`);
+    };
+}
+
+let  ivan = new User('Ivan', '18');//this в конструкторах и классах это новый экземпляр объекта
