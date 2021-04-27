469da1b9da6974854e48d568677e90571d87f9c9
Naran <lidzhiev-03@mail.ru>
2021-04-27 22:41:39 +0300

    контекст вызова 2

--- JS/script1.js
+++ JS/script1.js
@@ -5,6 +5,9 @@ const obj = {
   b:15,
   sum: function () {
       console.log(this);
+      function lapaka(){
+          console.log(this);//выводит undefined тк lapaka простая функция 
+      }
   }
 };
 obj.sum();
