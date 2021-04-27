ab1194bb1b41a2d0d8a225da7cf5749fc395e38b
Naran <lidzhiev-03@mail.ru>
2021-04-27 22:40:33 +0300

    контекст вызова 1

--- JS/script1.js
+++ JS/script1.js
@@ -1,23 +1,10 @@
-function User (name, id){
-    this.name =name;
-    this.id = id;
-    this.human = true;
-    this.hello = function(){
-        console.log(`Hello ${this.name}`);
-    }
-}
+'use strict'
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 19);
-
-
-console.log(ivan);
-console.log(alex);
-
-ivan.hello();
-alex.hello();
-
-User.prototype.exit = function(name){
-    console.log(`Пользователь ${this.name} выходит из конструкта`)
-}
-ivan.exit();
+const obj = {
+  a:20,
+  b:15,
+  sum: function () {
+      console.log(this);
+  }
+};
+obj.sum();
