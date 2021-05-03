d33bf8a53d562346c65357397a36a6d992c0ecd3
Naran <lidzhiev-03@mail.ru>
2021-05-03 14:47:31 +0300

    классы

--- JS/script1.js
+++ JS/script1.js
@@ -1,4 +1,25 @@
-const message = (function(){
-    const secret = "Здесь указан пароль!";
-    console.log(secret);
-})();
+class Rectangle {
+    constructor(height,width ){
+        this.height = height;
+        this.width = width;
+    }
+    calcArea(){
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle{
+     constructor(height, width, color, text){
+         super(height,width);
+         this.color =color;
+         this.text = text;
+     }
+     show(){
+         console.log(`text:${this.text} color: ${this.color}`);
+     }
+}
+
+
+const lowpoly = new ColoredRectangleWithText(100,200, 'red', 'hello');
+console.log(lowpoly.calcArea());
+lowpoly.show();
