01f47c7d2067d6102cc14615e50a3b98ce5b296b
Naran <lidzhiev-03@mail.ru>
2021-05-10 19:08:19 +0300

    значение по умолчанию

--- JS/script1.js
+++ JS/script1.js
@@ -5,4 +5,8 @@ function recreation(a,b, ...rest){
 
 recreation(12.342,2342342,12312,21,3,2,3,21,3,214,214,213,2,41,4,3,4);
 
+function calcOrDouble(number, base = 4){//тут мы присваиваем base значение по умолчанию
+    console.log(number*base);
+}
 
+calcOrDouble(7);
