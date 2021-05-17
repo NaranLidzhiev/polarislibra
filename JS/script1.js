1c44cecfacffdf580aeb4692e59d2778b77a5032
Naran <lidzhiev-03@mail.ru>
2021-05-17 12:40:21 +0300

    опа

--- JS/script1.js
+++ JS/script1.js
@@ -1,14 +1,2 @@
-const persone = {
-    name: 'Alex',
-    tel: '+79054091119',
-    parents: {
-        mom: 'Olga',
-        dad: 'Mike'
-    }
-};
+'use strict';
 
-const clon = JSON.parse(JSON.stringify(persone));//создаем глубокую копию которая абсолютно не зависит от persone 
-clon.parents.mom = 'Ann';
-persone.parents.mom = 'Ann';
-console.log(persone);
-console.log(clon);
