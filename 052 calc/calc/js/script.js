a393cb1e461ed7f2248bb0c47fcad1108a04aef1
Naran <lidzhiev-03@mail.ru>
2021-05-27 19:46:15 +0300

    книга

--- 052 calc/calc/js/script.js
+++ 052 calc/calc/js/script.js
@@ -6,23 +6,23 @@ const inputRub = document.querySelector('#rub'),
 inputRub.addEventListener('input', ()=>{
   const request = new XMLHttpRequest();
 
-  request.open('GET', 'js/current.json');
+  request.open('GET', 'js/current.json');//собирает настройки которые в будущем помогут сделать запрос 
   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   request.send();
 
 
-  request.addEventListener('readystatechange', () =>{//если изменится readystate то срабатаывает данный обработчик событий
+  request.addEventListener('load', () =>{//если изменится readystate то срабатаывает данный обработчик событий
     if ( request.status === 200 ){
-        console.log(request.response);
-        const data = JSON.parse(request.response);
-        inputUsd.value = (+inputRub.value/ data.current.usd).toFixed(2);
+        console.log(request.response);// получаем значение в виде JSON значения
+        const data = JSON.parse(request.response); // парсим - переводим значение из JSON вида в обычный вид
+        inputUsd.value = (+inputRub.value/ data.current.usd).toFixed(2);//переводим рубли в доллары toFixed округляет до 2 знаков после нуля
     } else {
         inputUsd.value = "Что-то пошло не так";
     }
   });
 
-  // status
-  // statusText
-  // response
-  // readyState
+  // status - статус нашего запроса 404 306 502 и тд
+  // statusText - текстовое описание ответа от сервера Ok Created Bad Gateway и тд
+  // response ответ от сервера
+  // readyState содержит текущее состояние нашего запроса
 });
