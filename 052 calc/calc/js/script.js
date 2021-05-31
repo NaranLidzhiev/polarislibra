5756f6fad2f0566152ae5fe1d5f8c2066ffd69de
Naran <lidzhiev-03@mail.ru>
2021-05-31 22:33:37 +0300

    Реализация скрипта отправки на сервер

--- 052 calc/calc/js/script.js
+++ 052 calc/calc/js/script.js
@@ -7,11 +7,11 @@ inputRub.addEventListener('input', ()=>{
   const request = new XMLHttpRequest();
 
   request.open('GET', 'js/current.json');//собирает настройки которые в будущем помогут сделать запрос 
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');//описывает вид файла
   request.send();
 
 
-  request.addEventListener('load', () =>{//если изменится readystate то срабатаывает данный обработчик событий
+  request.addEventListener('load', () =>{//если загрузится все то срабатаывает данный обработчик событий
     if ( request.status === 200 ){
         console.log(request.response);// получаем значение в виде JSON значения
         const data = JSON.parse(request.response); // парсим - переводим значение из JSON вида в обычный вид
